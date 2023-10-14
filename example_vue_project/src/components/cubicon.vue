94da5c691b052efa9e614c3994e8d7d083e79718
Haume <haume341@outlook.com>
2023-10-14 20:47:37 +0300

    Vue Public Icons v0.1

---
+++ example_vue_project/src/components/cubicon.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+let p = defineProps(["name", "class", "width", "height"]) // Props  (name, class, width, height)
+
+let i: any = ref("") // Icon variable
+async function get() {
+    let a = await fetch(`/${p.name}.svg`) // Fetching svg file
+    if (!a.ok) return
+    let b = await a.text() // Converting svg file to text
+    b = b.toString() // Converting svg file to string
+    const parser = new DOMParser(); // Parsing svg file
+    const svgDoc = parser.parseFromString(b, "image/svg+xml"); // Parsing svg file
+    if (p.width || p.height) { // If width or height is defined
+        svgDoc.documentElement.setAttribute("width", p.width)
+        svgDoc.documentElement.setAttribute("height", p.height)
+    } else { // If width or height is not defined
+        svgDoc.documentElement.removeAttribute("width");
+        svgDoc.documentElement.removeAttribute("height");
+    }
+    i.value = new XMLSerializer().serializeToString(svgDoc); // Writing svg file to variable
+}
+get() // Calling get function
+</script>
+
+<template>
+    <cubicon :class="p.class" v-if="i != ''" v-html="i"></cubicon>
+</template>
+
+<style scoped>
+cubicon { /* Core css rules */
+    display: flex;
+    justify-content: center;
+    flex-shrink: 0;
+}
+</style>
---
+++ example_vue_project/src/example_usage.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Cubicon from './components/cubicon.vue';
+</script>
+
+<template>
+  <h1 class="">Here's your icon!</h1>
+  <Cubicon name="lofodo" width="400" height="200" />
+</template>

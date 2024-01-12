bcc490a29eb307c29512d4df089d551c4aa54c7d
Haume <haume341@outlook.com>
2024-01-12 21:00:25 +0300

    Version 1.4.1

--- example_svelte_project/src/App.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import Cubicon from './lib/Icon.svelte';
-</script>
-
-<h1 class="">Here's your icon!</h1>
-<Cubicon name="cubidron" width="400" height="400" />
--- example_vue_project/src/app.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import Cubicon from './components/Icon.vue'
-</script>
-
-<template>
-  <h1 class="">Here's your icon!</h1>
-  <Cubicon name="lofodo" width="400" height="200" />
-</template>
